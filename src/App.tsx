0245bb955718568d0d7f5adfe9bdb5a932d8f6c6
Arpit Bhardwaj <arpitbhardwaj@Arpits-MacBook-Pro.local>
2021-06-15 18:12:32 +0530

    Deployment

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>arpitbhardwaj.com</p>
+        <p>arpitbhardwaj.com - dev</p>
       </header>
     </div>
   );
